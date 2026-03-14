42e7535b4009b0d95bd2bfe17300100cfc0b1ad9
Keenan1300 <hamilt25@sheridancollege.ca>
2026-03-14 19:30:20 -0400

    Added Funcitonal character controller, need to add jump function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody RB;
+    private Animator Anim;
+    public GameObject PlayerMesh;
     float horizontalinput;
     float VerticalInput;
     public float movespeed;
@@ -17,6 +19,7 @@ public class PlayerController : MonoBehaviour
     {
         RB = GetComponent<Rigidbody>();
         RB.freezeRotation = true;
+        Anim = PlayerMesh.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -45,6 +48,16 @@ public class PlayerController : MonoBehaviour
         //find move dir
         MoveDirection = Orientation.forward * VerticalInput + Orientation.right * horizontalinput;
         RB.AddForce(MoveDirection.normalized*movespeed*10f,ForceMode.Force);
+
+        //Animation
+        if (Input.GetAxisRaw("Horizontal") != 0 ||  Input.GetAxisRaw("Vertical") != 0)
+        {
+            Anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            Anim.SetBool("IsMoving", false);
+        }
 
     }
 
